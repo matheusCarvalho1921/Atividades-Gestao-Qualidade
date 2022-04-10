2c1e401c6d7e75a4aac3e809b155f5a0e3c15f7a
Matheus <matheuscarvalho1004@gmail.com>
2022-04-10 11:50:03 -0300

    Segundo commit do projeto: classe Java Teste vazia.

---
+++ pratica2/src/pratica2/Imc.java
@@ -0,0 +1,37 @@
+package pratica2;
+import java.util.Scanner;
+public class Imc {
+
+	public static void main(String[] args) {
+Scanner input= new Scanner(System.in);
+		
+
+double  imc, peso, altura;
+
+System.out.println("Digite o peso");
+peso = input.nextDouble();
+
+System.out.println("Digite a altura ");
+altura = input.nextDouble();
+
+imc = peso/(Math.pow(altura, 2));
+
+System.out.println(imc);
+
+
+if (imc<20) {
+	System.out.println("abaixo do peso");}
+else if (imc >=20 && imc <=25) {
+	System.out.println("Normal");
+	}
+else if (imc >=25 && imc <=30) {
+	System.out.println("sobrepeso");
+}
+
+else {
+	System.out.println("Obesidade");
+}
+
+	}
+
+}
