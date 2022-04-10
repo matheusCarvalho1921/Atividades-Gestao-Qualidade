27b47f5189f0c32e5b4e6178c1bb0ca979ce79b0
Matheus <matheuscarvalho1004@gmail.com>
2022-04-10 11:35:28 -0300

    Segundo commit do projeto: classe Java Teste vazia.

---
+++ pratica2/src/pratica2/DuasNotas.java
@@ -0,0 +1,29 @@
+package pratica2;
+import java.util.Scanner;
+public class DuasNotas {
+
+	public static void main(String[] args) {
+		
+			
+			 Scanner input = new  Scanner ( System . in );
+			
+			int  n1 , n2 , m ;
+			
+			System.out. println ( "digite a primeira nota:" );
+			n1 = input . nextInt ();
+			
+			System.out . println ( "digite a segunda nota:" );
+			n2 = input . nextInt ();
+			
+			m = ( n1 + n2 )/ 2 ;
+			
+			
+			if ( m >= 7 ) {
+				System.out.println( "Aprovado" );
+			}
+			else 
+				System.out.println( "Reprovado" );
+
+	}
+
+}
---
+++ pratica2/src/pratica2/Triangulo.java
@@ -0,0 +1,32 @@
+package pratica2;
+import java.util.Scanner;
+public class Triangulo {
+
+	public static void main(String[] args) {
+Scanner input= new Scanner(System.in);
+		
+		int l1,l2,l3;
+		
+		
+		System.out.println("Digite o primeiro lado");
+		l1 = input.nextInt();
+		
+		System.out.println("Digite o segundo lado");
+		l2 = input.nextInt();
+		
+		System.out.println("Digite o terceiro lado");
+		l3 = input.nextInt();
+		
+		if (l1==l2 && l2==l3) {
+			System.out.println("é equilátero");}
+		else if (l1==l2 || l2==l3||l3==l1){
+			System.out.println("é isósceles ");
+		}
+		else {
+			System.out.println("é escaleno");
+			
+		}
+
+	}
+
+}
