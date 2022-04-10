5aa2129110185aae51512e371d91aa868fd4b21d
Matheus <matheuscarvalho1004@gmail.com>
2022-04-10 10:12:09 -0300

    Primeiro commit do projeto: classe Java Teste vazia.

---
+++ pratica1/src/pratica1/AnoNascimento.java
@@ -0,0 +1,10 @@
+package pratica1;
+
+public class AnoNascimento {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ pratica1/src/pratica1/TresNotas.java
@@ -0,0 +1,27 @@
+package pratica1;
+import java.util.Scanner;
+
+public class TresNotas {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int a,b,c, media;
+		
+		System.out.println("Digite a primeira nota");
+		a = input.nextInt();
+		
+		System.out.println("Digite a segunda nota");
+		b = input.nextInt();
+		
+		System.out.println("Digite a terceira nota");
+		c = input.nextInt();
+		
+		media = (a+b+c)/3;
+		
+		System.out.println("A média é " +media);
+		
+
+	}
+
+}
