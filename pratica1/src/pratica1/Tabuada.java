b227abd8a0352426bc91b19f9117b838213dd9c2
Matheus <matheuscarvalho1004@gmail.com>
2022-04-10 11:10:46 -0300

    Primeiro commit do projeto: classe Java Teste vazia.

---
+++ pratica1/src/pratica1/Tabuada.java
@@ -0,0 +1,41 @@
+package pratica1;
+import java.util.Scanner;
+public class Tabuada {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int numero,tabuada0, tabuada1,tabuada2,tabuada3,tabuada4,tabuada5,tabuada6,tabuada7,tabuada8,tabuada9,tabuada10;
+		
+		System.out.println("digite seu número");
+		numero = input.nextInt();
+		
+		
+		tabuada0 = numero*0;
+		tabuada1 = numero *1;
+		tabuada2 = numero *2;
+		tabuada3 = numero *3;
+		tabuada4 = numero *4;
+		tabuada5 = numero *5;
+		tabuada6 = numero *6;
+		tabuada7 = numero *7;
+		tabuada8 = numero *8;
+		tabuada9 = numero *9;
+		tabuada10 = numero *10;
+		
+		
+		System.out.println(tabuada0);
+		System.out.println(tabuada1);
+		System.out.println(tabuada2);
+		System.out.println(tabuada3);
+		System.out.println(tabuada4);
+		System.out.println(tabuada5);
+		System.out.println(tabuada6);
+		System.out.println(tabuada7);
+		System.out.println(tabuada8);
+		System.out.println(tabuada9);
+		System.out.println(tabuada10);
+		
+	}
+
+}
